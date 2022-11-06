2f7fe22b2df4bd4f638fd64a74ec84735431810d
Aleksandr Dis <a.dis@awatera.com>
2022-11-06 23:34:21 +0400

    eslint init

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className='text 3x1 font-bold underline'>Hi</h1>
-    </div>
-  )
-}
+const Home = () => (
+  <div>
+    <h1 className="text 3x1 font-bold underline">Hi</h1>
+  </div>
+);
+
+export default Home;
